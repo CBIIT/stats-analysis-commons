61b8ae6e958a25b60d801e525b973d2b0a1cfb7c
no_author <no_author@no_author.cvs2svn>
2005-12-13 19:30:16 +0000

    This commit was manufactured by cvs2svn to create tag 'dev_14Dec05_0744PM'.
    SVN-Revision: 8739

---
+++ dev_14Dec05_0744PM/src/gov/nih/nci/caintegrator/exceptions/ValidationException.java
@@ -0,0 +1,14 @@
+package gov.nih.nci.caintegrator.exceptions;
+
+public class ValidationException extends FrameworkException{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public ValidationException(String string) {
+		super(string);
+	}
+
+}
