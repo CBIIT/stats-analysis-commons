8fe9d2ea9e1e723e776fd3effe961a3ecbb0082e
no_author <no_author@no_author.cvs2svn>
2005-12-13 18:14:58 +0000

    This commit was manufactured by cvs2svn to create tag 'caintegrator-analysis-commons_1_0_0_0_deploy-stage_build_8'.
    SVN-Revision: 8727

---
+++ caintegrator-analysis-commons_1_0_0_0_deploy-stage_build_8/src/gov/nih/nci/caintegrator/exceptions/FindingsQueryException.java
@@ -0,0 +1,14 @@
+package gov.nih.nci.caintegrator.exceptions;
+
+public class FindingsQueryException extends FrameworkException{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public FindingsQueryException(String string) {
+		super(string);
+	}
+
+}
