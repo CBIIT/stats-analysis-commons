37bb73719228e4b93a4cbcfa7599becbb915d714
no_author <no_author@no_author.cvs2svn>
2005-12-16 16:05:05 +0000

    This commit was manufactured by cvs2svn to create tag 'DEV_29DEC051942'.
    SVN-Revision: 8747

---
+++ DEV_29DEC051942/src/gov/nih/nci/caintegrator/enumeration/LinkageMethodType.java
@@ -0,0 +1,8 @@
+package gov.nih.nci.caintegrator.enumeration;
+
+import java.io.Serializable;
+
+public enum LinkageMethodType implements Serializable{
+	Average,Single,Complete;
+}
+
---
+++ DEV_29DEC051942/src/gov/nih/nci/caintegrator/exceptions/FindingsAnalysisException.java
@@ -0,0 +1,9 @@
+package gov.nih.nci.caintegrator.exceptions;
+
+public class FindingsAnalysisException extends FrameworkException {
+
+	public FindingsAnalysisException(String string) {
+		super(string);
+	}
+
+}
