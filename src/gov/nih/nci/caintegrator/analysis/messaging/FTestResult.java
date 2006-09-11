33d2a5869ebc7f8d74c7402e5c647153109b9aa1
Harris Michael <harrismic>
2006-09-11 17:02:45 +0000

    Filling out FTest classes.
    SVN-Revision: 8808

--- src/gov/nih/nci/caintegrator/analysis/messaging/FTestResult.java
+++ src/gov/nih/nci/caintegrator/analysis/messaging/FTestResult.java
@@ -1,7 +1,14 @@
 package gov.nih.nci.caintegrator.analysis.messaging;
 
+import java.util.List;
+
 public class FTestResult extends AnalysisResult {
 
+	private static final long serialVersionUID = 1L;
+	private List<FtestResultEntry> ftResultEntries;
+	private List<SampleGroup> sampleGroups;
+	private boolean arePvaluesAdjusted = false; 
+	
 	public FTestResult(String sessionId, String taskId) {
 		super(sessionId, taskId);
 		// TODO Auto-generated constructor stub
@@ -11,6 +18,30 @@ public class FTestResult extends AnalysisResult {
 	public String toString() {
 		// TODO Auto-generated method stub
 		return null;
+	}
+
+	public boolean arePvaluesAdjusted() {
+		return arePvaluesAdjusted;
+	}
+
+	public void setArePvaluesAdjusted(boolean arePvaluesAdjusted) {
+		this.arePvaluesAdjusted = arePvaluesAdjusted;
+	}
+
+	public List<FtestResultEntry> getFtResultEntries() {
+		return ftResultEntries;
+	}
+
+	public void setFtResultEntries(List<FtestResultEntry> ftResultEntries) {
+		this.ftResultEntries = ftResultEntries;
+	}
+
+	public List<SampleGroup> getSampleGroups() {
+		return sampleGroups;
+	}
+
+	public void setSampleGroups(List<SampleGroup> sampleGroups) {
+		this.sampleGroups = sampleGroups;
 	}
 
 }
---
+++ src/gov/nih/nci/caintegrator/analysis/messaging/FtestResultEntry.java
@@ -0,0 +1,42 @@
+package gov.nih.nci.caintegrator.analysis.messaging;
+
+import java.util.Collections;
+import java.util.List;
+
+public class FtestResultEntry implements java.io.Serializable {
+
+	private static final long serialVersionUID = 1L;
+	private List<Double> groupAve = Collections.emptyList();
+	private double maximumFoldChange;
+	private double pvalue;
+	
+	public FtestResultEntry() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public List<Double> getGroupAve() {
+		return groupAve;
+	}
+
+	public void setGroupAve(List<Double> groupAve) {
+		this.groupAve = groupAve;
+	}
+
+	public double getMaximumFoldChange() {
+		return maximumFoldChange;
+	}
+
+	public void setMaximumFoldChange(double maximumFoldChange) {
+		this.maximumFoldChange = maximumFoldChange;
+	}
+
+	public double getPvalue() {
+		return pvalue;
+	}
+
+	public void setPvalue(double pvalue) {
+		this.pvalue = pvalue;
+	}
+	 
+}
