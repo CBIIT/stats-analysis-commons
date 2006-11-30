6d6733529df08a88a0e04979c5c4de50c707a9e9
Harris Michael <harrismic>
2006-11-30 20:18:56 +0000

    Added toString method.
    SVN-Revision: 8880

--- src/gov/nih/nci/caintegrator/analysis/messaging/ClassComparisonLookupRequest.java
+++ src/gov/nih/nci/caintegrator/analysis/messaging/ClassComparisonLookupRequest.java
@@ -22,6 +22,36 @@ public class ClassComparisonLookupRequest extends ClassComparisonRequest {
 		this.reporterGroup = reporterGroup;
 	}
 	
-	
+	public String toString() {
+		
+		  int reporterGroupSize = -1;
+		  if (reporterGroup != null) {
+		    reporterGroupSize = reporterGroup.size();
+		  }
+		
+		  int blGroupSize = -1;
+		  SampleGroup baselineGroup = getBaselineGroup();
+		  if (baselineGroup!=null) {
+		    blGroupSize = baselineGroup.size();
+		  }
+		  
+		  SampleGroup group1 = getGroup1();
+		  int group1Size = -1;
+		  if (group1 !=null) {
+		    group1Size = group1.size();
+		  }
+			
+		  String retStr = "ClassComparisonAnalysisLookupRequest: sessionId=" + getSessionId() + " taskId=" + getTaskId() + " reporterGroupSize=" + reporterGroupSize +  " blGroupSize=" + blGroupSize + " group1Szie=" + group1Size;
+		  
+		  if (group1 != null) { 
+		    retStr += " GRP1=" + group1.getGroupName();
+		  }
+		  
+		  if (baselineGroup != null) {
+		    retStr += " baselineGroup=" + baselineGroup.getGroupName();
+		  }
+		  
+		  return retStr;
+		}
 
 }
